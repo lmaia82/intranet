86dbbe5f50a56465b1b0f4a2f669481d57d10b9c
lmaia82 <40031740+lmaia82@users.noreply.github.com>
2026-07-09 15:28:19 -0300

    Update configuration.php

--- lib/configuration.php
+++ lib/configuration.php
@@ -4,11 +4,11 @@ class DiamondConfig{
 	public $database = "intranet";
 	public $intranetName = "CETEM - Intra";
 	public $user = "root";
-	public $password = "Intr4n3t";
+	public $password = "__CHANGE_ME__";
 	public $friendly_url = false;
 	public $database_type = 0; //0 = MySql 1 = PostGreSQL
 #comentado em 08042015//	public $ldap_server = "mineral.cetem";
-	public $ldap_server = "172.16.0.53";
+	public $ldap_server = "__CHANGE_ME__";
 	public $ldap_tree = "OU=CETEM,DC=mineral,DC=cetem";
 }
 ?>
